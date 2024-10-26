26b879a7c6d2569e12df5b7c85858d9a9c04f5d9
Sebas455 <smolinal2300@alumno.ipn.mx>
2024-10-25 23:29:59 -0600

    Add files via upload

---
+++ decimalybinario.py
@@ -0,0 +1,33 @@
+def decimal_binario(decimal):
+    binario = ""  
+    while decimal > 0:
+        res = decimal % 2  
+        nuevobinario = str(res)  
+        binario = nuevobinario + binario
+        decimal = decimal // 2  
+    return binario
+
+def binario_decimal(binario):
+    decimal = 0
+    binario = binario[::-1] 
+    for i in range(len(binario)):
+        decimal += int(binario[i]) * (2 ** i) 
+    return decimal
+
+def menu():
+    print("1. Decimal a Binario")
+    print("2. Binario a Decimal")
+
+menu()
+opcion = input("Introduce la opción que quieres realizar").upper()
+
+if opcion == "1":
+    decimal = int(input("Ingrese el decimal:\n"))
+    binario = decimal_binario(decimal)
+    print("El número en binario es:", binario)
+elif opcion == "2":
+    binario = input("Ingrese el número binario:\n")
+    decimal = binario_decimal(binario)
+    print("El número en decimal es:", decimal)
+else:
+    print("Opción no válida.")
